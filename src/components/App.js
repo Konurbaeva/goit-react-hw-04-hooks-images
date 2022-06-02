87c49319580cf681b85a0a42aef01af06fe45769
Konurbaeva <konurbaevamadina@gmail.com>
2022-06-03 00:06:43 +0200

    fixed load more on App component

--- src/components/App.js
+++ src/components/App.js
@@ -13,12 +13,24 @@ import settings from "./settings";
 const { BASE_URL, API_KEY } = settings;
 axios.defaults.baseURL = BASE_URL;
 
-export default function App() {
 
+const APIfetch = async ({
+    searchQuery = '',
+    perPage = 7,
+    page = 1,
+} = {}) => {
+    const response = await axios
+        .get(
+            `?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&per_page=${perPage}&page=${page}`);
+    return response.data.hits;
+};
+
+
+export default function App() {
     const [hits, setHits] = useState([]);
     const [searchQuery, setSearchQuery] = useState('');
     const [page, setPage] = useState(1);
-    // const [currentPage, setCurrentPage] = useState(1);
+    const [currentPage, setCurrentPage] = useState(1);
     const [showModal, setShowModal] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
     //  const [totalHits, setTotalHits] = useState(0);
@@ -27,26 +39,30 @@ export default function App() {
     const [modalImage, setModalImage] = useState(null);
 
     const handleFormSubmit = queryFromSearchbar => {
-        // setSearchQuery({ searchQuery: queryFromSearchbar })
-
-        console.log('searchQuery: ', searchQuery)
+        console.log('queryFromSearchbar: ', queryFromSearchbar)
         setSearchQuery(queryFromSearchbar)
         setHits([])
         setPage(1)
     };
 
     useEffect(() => {
-        async function fetchData() {
-            const response = await axios.get(
-                `?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&per_page=${perPage}&page=${page}`
-            )
-            const data = await response.data.hits
-
-            setHits(data);
+        if (!searchQuery) {
+            return;
         }
 
-        fetchData();
-    }, [searchQuery, page, perPage]);
+        const loadResults = () => {
+            setIsLoading(true);
+
+            APIfetch({ searchQuery: searchQuery, currentPage })
+                .then(response => {
+                    setHits(prevResults => [...prevResults, ...response]);
+                })
+                .catch(error => setErrorMsg(error.message))
+                .finally(() => setIsLoading(false));
+        };
+
+        loadResults();
+    }, [searchQuery, currentPage]);
 
     const toggleModal = () => setShowModal(!showModal);
 
@@ -55,8 +71,9 @@ export default function App() {
         setModalImage(image);
     };
 
-    const loadMore = (prevState) => {
-        setPage({ page: prevState.page + 1 })
+
+    const loadMore = () => {
+        setCurrentPage(prevPage => prevPage + 1);
     };
 
     return (
